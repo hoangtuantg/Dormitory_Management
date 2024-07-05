211b15efe178098d987e0d32ef2b8babbf2e6e02
Vũ Văn Hoàng <6656485@sv.vnua.edu.vn>
2024-07-05 15:39:45 +0700

    db helper, first show data

--- XDPTPM-01-NHOM04-DOAN-HK2-23-24/Admin/Room.aspx.cs
+++ XDPTPM-01-NHOM04-DOAN-HK2-23-24/Admin/Room.aspx.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using XDPTPM_01_NHOM04_DOAN_HK2_23_24.Helpers;
 
 namespace XDPTPM_01_NHOM04_DOAN_HK2_23_24.Admin
 {
     public partial class Room : System.Web.UI.Page
     {
+
+        private DatabaseHelper helper = new DatabaseHelper();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            helper.openConnection();
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(helper.CreateCommand("select * from room_tbl"));
+            adapter.Fill(dataSet);
+            RoomList.DataSource = dataSet;
+            RoomList.DataBind();
+            helper.closeConnection();
         }
     }
 }
---
+++ XDPTPM-01-NHOM04-DOAN-HK2-23-24/Helpers/DatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace XDPTPM_01_NHOM04_DOAN_HK2_23_24.Helpers
+{
+    public class DatabaseHelper
+    {
+        private SqlConnection sqlConnection;
+
+        public DatabaseHelper() {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public void openConnection()
+        {
+            if(sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection() {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlCommand CreateCommand(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            return cmd;
+        }
+    }
+}

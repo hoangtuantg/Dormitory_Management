412e2b86dec41c3a16fc8638f36b0db57b0c2fec
Trương Tuấn Hoàng <6662012@sv.vnua.edu.vn>
2024-07-13 23:45:27 +0700

    up thieu commit trc

--- XDPTPM-01-NHOM04-DOAN-HK2-23-24/Admin/StudentList.aspx.cs
+++ XDPTPM-01-NHOM04-DOAN-HK2-23-24/Admin/StudentList.aspx.cs
@@ -159,7 +159,7 @@ namespace XDPTPM_01_NHOM04_DOAN_HK2_23_24.Admin
             gender.SelectedIndex = -1;
             MSV.Value = "";
             CCCD.Value = "";
-            //Dob.Value = "";
+            SDOB.Text = string.Empty;
             SRoom.Value = "";
             Key = 0;
         }
